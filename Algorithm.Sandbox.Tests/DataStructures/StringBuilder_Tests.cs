f39f7b95f92bcf9b4ff658a922de91d600d93b99
justcoding121 <jehonathant@gmail.com>
2016-07-31 17:44:58 -0400

    string builder tests

---
+++ Algorithm.Sandbox.Tests/DataStructures/StringBuilder_Tests.cs
@@ -0,0 +1,23 @@
+﻿using Algorithm.Sandbox.DataStructures;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Algorithm.Sandbox.Tests.DataStructures
+{
+    [TestClass]
+    public class StringBuilder_Tests
+    {
+        /// <summary>
+        /// A StringBuilder test
+        /// </summary>
+        [TestMethod]
+        public void StringBuilder_Test()
+        {
+            var builder = new AsStringBuilder();
+
+            builder.Append("Hello ");
+            builder.Append("J");
+
+            Assert.AreEqual(builder.ToString(), "Hello J");
+        }
+    }
+}
