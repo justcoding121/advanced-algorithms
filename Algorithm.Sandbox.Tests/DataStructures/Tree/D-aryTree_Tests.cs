8f00d4bd434732a1b5236d86a76e01d5d1358fee
justcoding121 <jehonathant@gmail.com>
2016-12-10 23:07:58 -0500

    Implement d-ary min heap

---
+++ Algorithm.Sandbox.Tests/DataStructures/Tree/D-aryTree_Tests.cs
@@ -0,0 +1,48 @@
+﻿using Algorithm.Sandbox.DataStructures;
+using Algorithm.Sandbox.DataStructures.Heap.Min;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Linq;
+
+namespace Algorithm.Sandbox.Tests.DataStructures
+{
+    [TestClass]
+    public class AsD_aryTree_Tests
+    {
+        /// <summary>
+        /// A tree test
+        /// </summary>
+        [TestMethod]
+        public void AsD_aryTree_Test()
+        {
+            //insert test
+            var tree = new AsD_aryMinHeap<int>(2);
+
+            for (int i = 99; i >= 0; i--)
+            {
+                tree.Insert(i);
+            }
+
+            for (int i = 0; i <= 99; i++)
+            {
+                var min = tree.ExtractMin();
+                Assert.AreEqual(min, i);
+            }
+
+            var rnd = new Random();
+            var testSeries = Enumerable.Range(1, 49).OrderBy(x => rnd.Next()).ToList();
+
+            foreach (var item in testSeries)
+            {
+                tree.Insert(item);
+            }
+
+            for (int i = 1; i <= 49; i++)
+            {
+                var min = tree.ExtractMin();
+                Assert.AreEqual(min, i);
+            }
+
+        }
+    }
+}
---
+++ Algorithm.Sandbox/DataStructures/Heap/Min/Asd-aryMinHeap.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Algorithm.Sandbox.DataStructures.Heap.Min
+{
+    public class AsD_aryMinHeap<T> where T : IComparable
+    {
+        private T[] heapArray;
+        private int K;
+        public int Count = 0;
+
+        public AsD_aryMinHeap(int k)
+        {
+            K = k;
+            this.heapArray = new T[k];
+        }
+
+        //O(log(n) base K)
+        public void Insert(T newItem)
+        {
+            if (Count == heapArray.Length)
+            {
+                doubleArray();
+            }
+
+            heapArray[Count] = newItem;
+
+            //percolate up
+            for (int i = Count; i > 0; i = (i - 1) / K)
+            {
+                if (heapArray[i].CompareTo(heapArray[(i - 1) / K]) < 0)
+                {
+                    var temp = heapArray[(i - 1) / K];
+                    heapArray[(i - 1) / K] = heapArray[i];
+                    heapArray[i] = temp;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            Count++;
+        }
+        //O(log(n) base K)
+        public T ExtractMin()
+        {
+            if (Count == 0)
+            {
+                throw new Exception("Empty heap");
+            }
+            var min = heapArray[0];
+
+            //move last element to top
+            heapArray[0] = heapArray[Count - 1];
+            Count--;
+
+            int currentParent = 0;
+            //now percolate down
+            while (true)
+            {
+                var swapped = false;
+
+                //init to left-most child
+                var minChildIndex = findMinChildIndex(currentParent);
+
+                if (minChildIndex!=-1 &&
+                    heapArray[currentParent].CompareTo(heapArray[minChildIndex]) > 0)
+                {
+                    var tmp = heapArray[minChildIndex];
+                    heapArray[minChildIndex] = heapArray[currentParent];
+                    heapArray[currentParent] = tmp;
+                    swapped = true;
+                }
+
+                if (!swapped)
+                {
+                    break;
+                }
+
+                currentParent = minChildIndex;
+
+            }
+
+            if (heapArray.Length / 2 == Count && heapArray.Length > 2)
+            {
+                halfArray();
+            }
+
+            return min;
+        }
+
+        private int findMinChildIndex(int currentParent)
+        {
+            var currentMin = currentParent * K + 1;
+
+            if (currentMin >= Count)
+                return -1;
+
+            for (int i = 2; i <= K; i++)
+            {
+                var nextSibling = heapArray[currentParent * K + i];
+
+                if (currentMin >= Count)
+                    break;
+
+                if (heapArray[currentMin].CompareTo(nextSibling) > 0)
+                {
+                    currentMin = currentParent * K + i;
+                }
+            }
+            return currentMin;
+        }
+
+        //o(1)
+        public T PeekMin()
+        {
+            if (Count == 0)
+            {
+                throw new Exception("Empty heap");
+            }
+
+            return heapArray[0];
+        }
+
+        private void halfArray()
+        {
+            var smallerArray = new T[heapArray.Length / 2];
+
+            for (int i = 0; i < Count; i++)
+            {
+                smallerArray[i] = heapArray[i];
+            }
+
+            heapArray = smallerArray;
+        }
+
+        private void doubleArray()
+        {
+            var biggerArray = new T[heapArray.Length * 2];
+
+            for (int i = 0; i < Count; i++)
+            {
+                biggerArray[i] = heapArray[i];
+            }
+
+            heapArray = biggerArray;
+        }
+    }
+}
