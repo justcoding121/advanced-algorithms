22f991ca1989670f78ee3f9ddb88c089a213f04f
justcoding121 <jehonathant@gmail.com>
2017-02-15 17:50:33 -0500

    Stub Pairing Heap

--- Algorithm.Sandbox/DataStructures/Heap/Min/AsPairingMinHeap.cs
+++ Algorithm.Sandbox/DataStructures/Heap/Min/AsPairingMinHeap.cs
@@ -1,12 +1,74 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Algorithm.Sandbox.DataStructures
 {
-    class AsPairingHeap
+    public class AsPairingTreeNode<T> : IComparable where T : IComparable
     {
+        internal T Value { get; set; }
+        internal int Degree;
+
+        internal AsDoublyLinkedList<AsPairingTreeNode<T>> Children { get; set; }
+
+        public AsPairingTreeNode(T value)
+        {
+            this.Value = value;
+            Children = new AsDoublyLinkedList<AsPairingTreeNode<T>>();
+        }
+
+        public int CompareTo(object obj)
+        {
+            return this.Value.CompareTo((obj as AsPairingTreeNode<T>).Value);
+        }
     }
+
+    public class AsPairingMinHeap<T> where T : IComparable
+    {
+        internal AsPairingTreeNode<T> Root;
+
+        internal int Count { get; private set; }
+
+        public AsPairingTreeNode<T> Insert(T newItem)
+        {
+            var newNode = new AsPairingTreeNode<T>(newItem);
+
+            throw new NotImplementedException();
+
+        }
+        private void Meld()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T ExtractMin()
+        {
+
+            throw new NotImplementedException();
+        }
+
+
+        public void DecrementKey(AsPairingTreeNode<T> node)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Union(AsPairingMinHeap<T> PairingHeap)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        private void MergeForests(AsDoublyLinkedList<AsPairingTreeNode<T>> newHeapForest)
+        {
+
+            throw new NotImplementedException();
+
+        }
+
+        public T PeekMin()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+
 }
