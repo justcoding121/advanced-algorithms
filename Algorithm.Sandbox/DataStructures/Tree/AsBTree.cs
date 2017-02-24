48c94f78a10fb6fc0e4b5355e7047969e6ad7d36
justcoding121 <jehonathant@gmail.com>
2017-02-23 23:43:51 -0500

    update progress

--- Algorithm.Sandbox/DataStructures/Tree/AsBTree.cs
+++ Algorithm.Sandbox/DataStructures/Tree/AsBTree.cs
@@ -4,11 +4,17 @@ namespace Algorithm.Sandbox.DataStructures.Tree
 {
     internal class AsBTreeNode<T> where T : IComparable
     {
-        public AsArrayList<T> Value { get; set; }
+        public T[] Values { get; set; }
 
         public AsBTreeNode<T> Parent { get; set; }
-        public AsArrayList<AsBTreeNode<T>> Children { get; set; }
+        public AsBTreeNode<T>[] Children { get; set; }
+        public int Count { get; internal set; }
 
+        public AsBTreeNode(int maxChildren)
+        {
+            Values = new T[maxChildren];
+            Children = new AsBTreeNode<T>[maxChildren + 1];
+        }
     }
 
     public class AsBTree<T> where T : IComparable
@@ -16,10 +22,33 @@ namespace Algorithm.Sandbox.DataStructures.Tree
         public int Count { get; private set; }
 
         internal AsBTreeNode<T> Root;
+        private int maxChildren;
+
+        public AsBTree(int maxChildren)
+        {
+            this.maxChildren = maxChildren;
+        }
+
 
         public void Insert(T value)
         {
+            var nodeToInsert = FindInsertionNode(ref Root, value);
 
+            if (nodeToInsert.Count == nodeToInsert.Values.Length)
+            {
+
+            }
+        }
+
+        private AsBTreeNode<T> FindInsertionNode(ref AsBTreeNode<T> node, T value)
+        {
+            if (node == null)
+            {
+                node = new AsBTreeNode<T>(maxChildren);
+                return node;
+            }
+
+            throw new NotImplementedException();
         }
 
         public void Delete(T value)
