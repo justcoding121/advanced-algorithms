c4743327b4bd85b866d05eab56dd6cacfa588b10
justcoding121 <jehonathant@gmail.com>
2017-03-25 22:33:08 -0400

    Segment tree

---
+++ Algorithm.Sandbox.Tests/DataStructures/Tree/SegmentTree_Tests.cs
@@ -0,0 +1,28 @@
+﻿using Algorithm.Sandbox.DataStructures;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Algorithm.Sandbox.Tests.DataStructures.Tree
+{
+    [TestClass]
+    public class AsSegmentTreeTests
+    {
+        /// <summary>
+        /// Smoke test
+        /// </summary>
+        [TestMethod]
+        public void AsSegmentTree_Sum_Smoke_Test()
+        {
+            var testArray = new int[] { 1, 3, 5, 7, 9, 11 };
+          
+            //tree with sum operation
+            var tree = new AsSegmentTree<int>(testArray,
+                new Func<int, int, int>((x, y) => x + y),
+                new Func<int>(() => 0));
+
+            var sum = tree.GetRangeResult(1, 3);
+
+            Assert.AreEqual(15, sum);
+        }
+    }
+}
