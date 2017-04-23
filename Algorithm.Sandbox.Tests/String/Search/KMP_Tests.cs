2d0439ac8f8be985a0a98644129b18a5936cd1cf
justcoding121 <jehonathant@gmail.com>
2017-04-22 22:40:12 -0400

    KMP implementation

---
+++ Algorithm.Sandbox.Tests/String/Search/KMP_Tests.cs
@@ -0,0 +1,32 @@
+﻿using Algorithm.Sandbox.String.Search;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm.Sandbox.Tests.String.Search
+{
+    [TestClass]
+    public class KMP_Tests
+    {
+        [TestMethod]
+        public void String_KMP_Test()
+        {
+            var kmpAlgo = new KMP();
+
+            var index = kmpAlgo.Search("abdcdabcaabcdabcag", "abcdabca");
+
+            Assert.AreEqual(9, index);
+
+            index = kmpAlgo.Search("abcabababdefgabcd", "fgg");
+
+            Assert.AreEqual(-1, index);
+
+            index = kmpAlgo.Search("abxabcabcaby", "abcaby");
+
+            Assert.AreEqual(6, index);
+        }
+    }
+}
---
+++ Algorithm.Sandbox/String/Search/KMP.cs
@@ -0,0 +1,94 @@
+﻿namespace Algorithm.Sandbox.String.Search
+{
+    /// <summary>
+    /// Knuth–Morris–Pratt(KMP) search implementation
+    /// </summary>
+    public class KMP
+    {
+        /// <summary>
+        /// Returns the start index of first appearance
+        /// of pattern in input string
+        /// returns -1 if no match
+        /// </summary>
+        /// <param name="input"></param>
+        /// <param name="pattern"></param>
+        /// <returns></returns>
+        public int Search(string input, string pattern)
+        {
+            var matchingInProgress = false;
+            var matchIndex = new int[pattern.Length];
+            var j = 0;
+
+            //create match index of chars
+            //to keep track of closest suffixes in pattern that form prefix of pattern
+            for (int i = 1; i < pattern.Length; i++)
+            {
+                //prefix don't match suffix anymore
+                if (!pattern[i].Equals(pattern[j]))
+                {
+                    //don't skip unmatched i for next iteration
+                    //since our for loop increments i
+                    if(matchingInProgress)
+                    {
+                        i--;
+                    }
+
+                    matchingInProgress = false;
+
+                    //move back j to the beginning of last matched char
+                    j = matchIndex[j == 0? 0:j-1];
+                }
+                //prefix match suffix so far
+                else
+                {
+                    matchingInProgress = true;
+                    //increment index of suffix 
+                    //to prefix index for corresponding char
+                    matchIndex[i] = j + 1;
+                    j++;
+                }
+            }
+
+            matchingInProgress = false;
+            //now start matching
+            j = 0;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == pattern[j])
+                {
+                    matchingInProgress = true;
+                    j++;
+
+                    //match complete
+                    if (j == pattern.Length)
+                    {
+                        return i - pattern.Length + 1;
+                    }
+
+                }
+                else
+                {
+                    //reduce i by one so that next comparison won't skip current i
+                    //which is not matching with current j
+                    //since our for loop increments i
+                    if (matchingInProgress)
+                    {
+                        i--;
+                    }
+
+                    matchingInProgress = false;
+
+                    //jump back to closest suffix with prefix of pattern
+                    if (j != 0)
+                    {
+                        j = matchIndex[j - 1];
+                    }
+
+                }
+            }
+
+            return -1;
+        }
+    }
+}
