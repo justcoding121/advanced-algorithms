d0ec80511cb5629e87fa23450860e00541e71340
justcoding121 <jehonathant@gmail.com>
2017-05-05 01:09:50 -0400

    bool parenthesis count

---
+++ Algorithm.Sandbox.Tests/DynamicProgramming/BoolParenthesis_Tests.cs
@@ -0,0 +1,28 @@
+﻿using Algorithm.Sandbox.DynamicProgramming;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm.Sandbox.Tests.DynamicProgramming
+{
+    [TestClass]
+    public class BoolParenthesis_Tests
+    {
+        [TestMethod]
+        public void Smoke_Test_BoolParenthesis()
+        {
+            //T|F^F&T|F^F^F^T|T&T^T|F^T^F&F^T|T^F
+            var result = CountBoolParenthesization.CountPositiveCombinations(
+                new bool[] {
+               true, false, false, true, false, false, false, true,
+               true, true, true, false, true, false, false, true, true, false
+                },
+                new char[] { '|', '^', '&', '|', '^', '^', '^', '|', '&', '^', '|', '^', '^', '&', '^', '|', '^' });
+
+            Assert.AreEqual(99632640, result);
+        }
+    }
+}
---
+++ Algorithm.Sandbox/DynamicProgramming/CountBoolParenthesization.cs
@@ -0,0 +1,209 @@
+﻿using Algorithm.Sandbox.DataStructures;
+using System;
+using System.Linq;
+
+namespace Algorithm.Sandbox.DynamicProgramming
+{
+    public class CountBoolParenthesization
+    {
+        public static int CountPositiveCombinations
+            (bool[] symbols, char[] operators)
+        {
+            if (symbols.Length <= 1 || operators.Length == 0)
+            {
+                throw new Exception("Invalid symbol or operator lengths.");
+            }
+
+            if (symbols.Length != operators.Length + 1)
+            {
+                throw new System.Exception("Operator count should be one less than symbol length.");
+            }
+
+            var possibleOperators = new char[] { '&', '^', '|' };
+
+            if (operators.Any(x => !possibleOperators.Contains(x)))
+            {
+                throw new Exception("Not all operators are supported.");
+            }
+
+            return True(symbols, operators, 0, symbols.Length - 1, 
+                        new AsDictionary<string, int>(), 
+                        new AsDictionary<string, int>(),
+                        new AsDictionary<string, int>());
+
+        }
+
+        /// <summary>
+        /// Returns total number of true result paranthesis combinations between indices i & j
+        /// </summary>
+        /// <param name="symbols"></param>
+        /// <param name="operators"></param>
+        /// <param name="i"></param>
+        /// <param name="j"></param>
+        /// <returns></returns>
+        private static int True(bool[] symbols,
+            char[] operators,
+            int i, int j, 
+            AsDictionary<string, int> trueCache,
+            AsDictionary<string, int> falseCache,
+            AsDictionary<string, int> bothCache)
+        {
+            if (j < i)
+            {
+                return 0;
+            }
+
+            if (j == i)
+            {
+                if (symbols[i])
+                {
+                    return 1;
+                }
+
+                return 0;
+            }
+
+            var cacheKey = string.Concat(i, j);
+
+            if (trueCache.ContainsKey(cacheKey))
+            {
+                return trueCache[cacheKey];
+            }
+
+            var result = 0;
+
+            //k is the separator between i & j
+            for (int k = i; k < j; k++)
+            {
+                switch (operators[k])
+                {
+                    case '&':
+                        result += True(symbols, operators, i, k, trueCache, falseCache, bothCache)
+                            * True(symbols, operators, k + 1, j, trueCache, falseCache, bothCache);
+                        break;
+
+                    case '|':
+                        result += Both(symbols, operators, i, k, trueCache, falseCache, bothCache)
+                            * Both(symbols, operators, k + 1, j, trueCache, falseCache, bothCache)
+                            - False(symbols, operators, i, k, trueCache, falseCache, bothCache) 
+                            * False(symbols, operators, k + 1, j, trueCache, falseCache, bothCache);
+                        break;
+
+                    case '^':
+                        result += True(symbols, operators, i, k, trueCache, falseCache, bothCache)
+                            * False(symbols, operators, k + 1, j, trueCache, falseCache, bothCache)
+                            + False(symbols, operators, i, k, trueCache, falseCache, bothCache) 
+                            * True(symbols, operators, k + 1, j, trueCache, falseCache, bothCache);
+                        break;
+                }
+            }
+
+            trueCache.Add(cacheKey, result);
+
+            return result;
+
+        }
+
+
+        /// <summary>
+        /// Returns total number of false result paranthesis combinations between indices i & j
+        /// </summary>
+        /// <param name="symbols"></param>
+        /// <param name="operators"></param>
+        /// <param name="i"></param>
+        /// <param name="j"></param>
+        /// <returns></returns>
+        private static int False(bool[] symbols,
+           char[] operators,
+           int i, int j,
+           AsDictionary<string, int> trueCache,
+           AsDictionary<string, int> falseCache,
+           AsDictionary<string, int> bothCache)
+        {
+            if (j < i)
+            {
+                return 0;
+            }
+
+            if (j == i)
+            {
+                if (!symbols[i])
+                {
+                    return 1;
+                }
+
+                return 0;
+            }
+
+            var cacheKey = string.Concat(i, j);
+
+            if (falseCache.ContainsKey(cacheKey))
+            {
+                return falseCache[cacheKey];
+            }
+
+            var result = 0;
+
+            //k is the separator between i & j
+            for (int k = i; k < j; k++)
+            {
+                switch (operators[k])
+                {
+                    case '&':
+                        result += Both(symbols, operators, i, k, trueCache, falseCache, bothCache) 
+                            * Both(symbols, operators, k + 1, j, trueCache, falseCache, bothCache)
+                             - True(symbols, operators, i, k, trueCache, falseCache, bothCache) 
+                             * True(symbols, operators, k + 1, j, trueCache, falseCache, bothCache);
+                        break;
+
+                    case '|':
+                        result += False(symbols, operators, i, k, trueCache, falseCache, bothCache)
+                            * False(symbols, operators, k + 1, j, trueCache, falseCache, bothCache);
+                        break;
+
+                    case '^':
+                        result += True(symbols, operators, i, k, trueCache, falseCache, bothCache) 
+                            * True(symbols, operators, k + 1, j, trueCache, falseCache, bothCache)
+                            + False(symbols, operators, i, k, trueCache, falseCache, bothCache) 
+                            * False(symbols, operators, k + 1, j, trueCache, falseCache, bothCache);
+                        break;
+                }
+            }
+
+            falseCache.Add(cacheKey, result);
+
+            return result;
+
+        }
+
+        /// <summary>
+        /// Returns total number of true & false result paranthesis combinations between indices i & j
+        /// </summary>
+        /// <param name="symbols"></param>
+        /// <param name="operators"></param>
+        /// <param name="i"></param>
+        /// <param name="j"></param>
+        /// <returns></returns>
+        private static int Both(bool[] symbols,
+           char[] operators,
+           int i, int j,
+           AsDictionary<string, int> trueCache,
+           AsDictionary<string, int> falseCache,
+           AsDictionary<string, int> bothCache)
+        {
+            var cacheKey = string.Concat(i, j);
+
+            if (bothCache.ContainsKey(cacheKey))
+            {
+                return bothCache[cacheKey];
+            }
+
+            var result = True(symbols, operators, i, j, trueCache, falseCache, bothCache)
+                + False(symbols, operators, i, j, trueCache, falseCache, bothCache);
+
+            bothCache.Add(cacheKey, result);
+
+            return result;
+        }
+    }
+}
