50fc858331905551c239e087cabb83a3342afb11
justcoding121 <jehonathant@gmail.com>
2017-05-11 23:06:43 -0400

    fix longest increasing sequence

--- Algorithm.Sandbox.Tests/DynamicProgramming/Sequence/LongestIncreasingSubSequence_Tests.cs
+++ Algorithm.Sandbox.Tests/DynamicProgramming/Sequence/LongestIncreasingSubSequence_Tests.cs
@@ -13,9 +13,17 @@ namespace Algorithm.Sandbox.Tests.DynamicProgramming
         [TestMethod]
         public void LongestIncreasingSubSequence_Smoke_Test()
         {
-           Assert.AreEqual(6, 
-                LongestIncreasingSubSequence
-                .FindSequence(new int[] { 85, 27, 14, 38, 26, 55, 46, 65, 85 }));
+            Assert.AreEqual(6, 
+                LongestIncreasingSubSequence.FindSequence(new int[] { 15, 27, 14, 38, 26, 55, 46, 65, 85 }));
+
+            Assert.AreEqual(4,
+                LongestIncreasingSubSequence.FindSequence(new int[] { 50, 3, 10, 7, 40, 80 }));
+
+            Assert.AreEqual(1,
+              LongestIncreasingSubSequence.FindSequence(new int[] { 3, 2 }));
+
+            Assert.AreEqual(2,
+              LongestIncreasingSubSequence.FindSequence(new int[] { 2, 3 }));
 
             Assert.AreEqual(6,
                 LongestIncreasingSubSequence.FindSequence(new int[] { 10, 22, 9, 33, 21, 50, 41, 60, 80 }));
--- Algorithm.Sandbox/DynamicProgramming/Sequence/LongestIncreasingSubSequence.cs
+++ Algorithm.Sandbox/DynamicProgramming/Sequence/LongestIncreasingSubSequence.cs
@@ -26,7 +26,30 @@ namespace Algorithm.Sandbox.DynamicProgramming
         /// <returns></returns>
         private static int FindSequence(int[] input, int j, ref int maxLength)
         {
-            throw new NotImplementedException();
+            if (j == 0)
+            {
+                return 1;
+            }
+
+            var currentMaxLength = 1;
+
+            for (int i = 0; i < j; i++)
+            {
+                //from 0 to i
+                var subMaxLength = FindSequence(input, i, ref maxLength);
+
+                //if 0 to i sequence last value (i) is less than current value j
+                //And if it improves our max
+                if (input[i] < input[j]
+                    && currentMaxLength < subMaxLength + 1)
+                {
+                    currentMaxLength = subMaxLength + 1;
+                }
+            }
+
+            maxLength = Math.Max(maxLength, currentMaxLength);
+
+            return currentMaxLength;
         }
     }
 }
