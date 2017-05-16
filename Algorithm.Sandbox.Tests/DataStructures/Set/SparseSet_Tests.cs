2a17dff834e32bbc96c91f5049fae32a9d0ee90c
justcoding121 <jehonathant@gmail.com>
2017-05-16 16:59:21 -0400

    Sparse Set implementation

---
+++ Algorithm.Sandbox.Tests/DataStructures/Set/SparseSet_Tests.cs
@@ -0,0 +1,57 @@
+﻿using Algorithm.Sandbox.DataStructures.Set;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm.Sandbox.Tests.DataStructures.Set
+{
+    [TestClass]
+    public class SparseSet_Tests
+    {
+        [TestMethod]
+        public void SparseSet_Smoke_Test()
+        {
+            var set = new SparseSet(15, 10);
+
+            set.Add(6);
+            set.Add(15);
+            set.Add(0);
+
+            set.Remove(15);
+
+            Assert.IsTrue(set.HasItem(6));
+            Assert.AreEqual(2, set.Length);
+        }
+
+        [TestMethod]
+        public void SparseSet_Stress_Test()
+        {
+            var set = new SparseSet(1000, 1000);
+
+            var random = new Random();
+            var testCollection = Enumerable.Range(0, 1000)
+                .OrderBy(x => random.Next())
+                .ToList();
+
+            foreach(var element in testCollection)
+            {
+                set.Add(element);
+            }
+
+            foreach (var element in testCollection)
+            {
+                Assert.IsTrue(set.HasItem(element));
+            }
+
+            foreach (var element in testCollection)
+            {
+                Assert.IsTrue(set.HasItem(element));
+                set.Remove(element);
+                Assert.IsFalse(set.HasItem(element));
+            }
+        }
+    }
+}
---
+++ Algorithm.Sandbox/DataStructures/Set/SparseSet.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Linq;
+
+namespace Algorithm.Sandbox.DataStructures.Set
+{
+    public class SparseSet
+    {
+        private int[] sparse;
+        private int[] dense;
+
+        public int Length { get; private set; }
+
+        public SparseSet(int maxVal, int capacity)
+        {
+            sparse = Enumerable.Repeat(-1, maxVal + 1).ToArray(); 
+            dense = Enumerable.Repeat(-1, capacity).ToArray();
+        }
+
+        public void Add(int value)
+        {
+            if (value < 0)
+            {
+                throw new Exception("Negative values not supported.");
+            }
+
+            if (value >= sparse.Length)
+            {
+                throw new Exception("Item is greater than max value.");
+            }
+
+            if (Length >= dense.Length)
+            {
+                throw new Exception("Set reached its capacity.");
+            }
+
+            sparse[value] = Length;
+            dense[Length] = value;
+            Length++;
+        }
+
+        public void Remove(int value)
+        {
+            if (value < 0)
+            {
+                throw new Exception("Negative values not supported.");
+            }
+
+            if (value >= sparse.Length)
+            {
+                throw new Exception("Item is greater than max value.");
+            }
+
+            if (HasItem(value) == false)
+            {
+                throw new Exception("Item do not exist.");
+            }
+
+            //find element
+            var index = sparse[value];
+            sparse[value] = -1;
+
+            //replace index with last value of dense
+            var lastVal = dense[Length - 1];
+            dense[index] = lastVal;
+            dense[Length - 1] = -1;
+
+            //update sparse for lastVal
+            sparse[lastVal] = index;
+
+            Length--;
+        }
+
+        public bool HasItem(int value)
+        {
+            var index = sparse[value];
+
+            if(index == -1 || dense[index] != value)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public void Clear()
+        {
+            Length = 0;
+        }
+    }
+}
