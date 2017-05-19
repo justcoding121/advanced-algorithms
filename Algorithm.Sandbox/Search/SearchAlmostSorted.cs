3f42b3aae916605ec732b85861628d7072c5930c
justcoding121 <jehonathant@gmail.com>
2017-05-19 17:39:04 -0400

    almost sorted search & sort

--- Algorithm.Sandbox/Search/SearchAlmostSorted.cs
+++ Algorithm.Sandbox/Search/SearchAlmostSorted.cs
@@ -4,9 +4,55 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Algorithm.Sandbox.Sorting
+namespace Algorithm.Sandbox.Search
 {
-    class SearchAlmostSorted
+    /// <summary>
+    /// Problem statement below
+    /// http://www.geeksforgeeks.org/search-almost-sorted-array/
+    /// </summary>
+    public class SearchAlmostSorted
     {
+        public static int Search(int[] input, int element)
+        {
+            return SearchRecursive(input, 0, input.Length - 1, element);
+        }
+
+        private static int SearchRecursive(int[] input, int i, int j, int element)
+        {
+            if (i == j)
+            {
+                if (input[i] == element)
+                {
+                    return i;
+                }
+
+                return -1;
+            }
+
+            var mid = (i + j) / 2;
+
+            if (input[mid] == element)
+            {
+                return mid;
+            }
+
+            if (mid > 0 && input[mid - 1] == element)
+            {
+                return mid - 1;
+            }
+
+            if (mid < input.Length - 1 && input[mid + 1] == element)
+            {
+                return mid + 1;
+            }
+
+            if (input[mid] > element)
+            {
+                return SearchRecursive(input, i, mid, element);
+            }
+
+            return SearchRecursive(input, mid + 1, j, element);
+
+        }
     }
 }
--- Algorithm.Sandbox/Sorting/SortAlmostSorted.cs
+++ Algorithm.Sandbox/Sorting/SortAlmostSorted.cs
@@ -1,12 +1,48 @@
-﻿using System;
+﻿using Algorithm.Sandbox.DataStructures;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Algorithm.Sandbox.Sorting
 {
-    class SortAlmostSorted
+    /// <summary>
+    /// Problem statement below
+    /// http://www.geeksforgeeks.org/nearly-sorted-algorithm/
+    /// </summary>
+    public class SortAlmostSorted
     {
+        /// <summary>
+        /// Sort the given input
+        /// where elements are misplaced almost by a distance k
+        /// </summary>
+        /// <param name="input"></param>
+        /// <param name="k"></param>
+        /// <returns></returns>
+        public static int[] Sort(int[] input, int k)
+        {
+            if (k > input.Length)
+            {
+                throw new Exception("K cannot exceed array length.");
+            }
+
+            var firstKElements = new int[k];
+            Array.Copy(input, firstKElements, k);
+
+            var minHeap = new BMinHeap<int>(firstKElements);
+
+            var result = new List<int>();
+
+            for (int i = k; i < input.Length; i++)
+            {
+                result.Add(minHeap.ExtractMin());
+                minHeap.Insert(input[i]);
+            }
+
+            while (minHeap.Count > 0)
+            {
+                result.Add(minHeap.ExtractMin());
+            }
+
+            return result.ToArray();
+        }
     }
 }
