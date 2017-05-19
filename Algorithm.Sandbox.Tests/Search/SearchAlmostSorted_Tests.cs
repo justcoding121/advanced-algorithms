1586d6c8ed8d61395df15247f27d5f2164cc9b90
justcoding121 <jehonathant@gmail.com>
2017-05-19 17:43:11 -0400

    add tests for sort

---
+++ Algorithm.Sandbox.Tests/Search/SearchAlmostSorted_Tests.cs
@@ -0,0 +1,23 @@
+﻿using Algorithm.Sandbox.Search;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Algorithm.Sandbox.Tests.Search
+{
+    /// <summary>
+    /// Problem statement below
+    /// http://www.geeksforgeeks.org/search-almost-sorted-array/
+    /// </summary>
+    [TestClass]
+    public class SearchAlmostSorted_Tests
+    {
+        [TestMethod]
+        public void Search_Almost_Sorted_Smoke_Test()
+        {
+            var test = new int[] { 10, 3, 40, 20, 50, 80, 70 };
+
+            Assert.AreEqual(2, SearchAlmostSorted.Search(test, 40));
+            Assert.AreEqual(-1, SearchAlmostSorted.Search(test, 100));
+
+        }
+    }
+}
---
+++ Algorithm.Sandbox.Tests/Sorting/SortAlmostSorted_Tests.cs
@@ -0,0 +1,25 @@
+﻿using Algorithm.Sandbox.Sorting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Algorithm.Sandbox.Tests.Sorting
+{
+    /// <summary>
+    /// Problem statement below
+    /// http://www.geeksforgeeks.org/nearly-sorted-algorithm/
+    /// </summary>
+    [TestClass]
+    public class SortAlmostSorted_Tests
+    {
+        private static int[] testArray =
+            new int[] { 2, 6, 3, 12, 56, 8 };
+
+        /// <summary>
+        /// </summary>
+        [TestMethod]
+        public void SortAlmostSorted_Smoke_Test()
+        {
+            CollectionAssert.AreEqual(new int[] { 2, 3, 6, 8, 12, 56 },
+                SortAlmostSorted.Sort(new int[] { 2, 6, 3, 12, 56, 8 }, 3));
+        }
+    }
+}
