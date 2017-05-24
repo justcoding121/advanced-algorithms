8a5b48666ced726f1c2d9a2c22789b1e5460c462
justcoding121 <jehonathant@gmail.com>
2017-05-24 14:37:30 -0400

    Count binary trees

---
+++ Algorithm.Sandbox.Tests/DynamicProgramming/Count/CountBinaryTree_Tests.cs
@@ -0,0 +1,28 @@
+﻿using Algorithm.Sandbox.DynamicProgramming.Count;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm.Sandbox.Tests.DynamicProgramming.Count
+{
+    /// <summary>
+    /// Problem details below
+    /// http://www.geeksforgeeks.org/find-all-possible-trees-with-given-inorder-traversal/
+    /// </summary>
+    [TestClass]
+    public class CountBinaryTree_Tests
+    {
+        [TestMethod]
+        public void Count_Smoke_Test()
+        {
+            Assert.AreEqual(1, CountBinaryTree.Count(1));
+            Assert.AreEqual(2, CountBinaryTree.Count(2));
+            Assert.AreEqual(5, CountBinaryTree.Count(3));
+            Assert.AreEqual(14, CountBinaryTree.Count(4));
+            Assert.AreEqual(42, CountBinaryTree.Count(5));
+        }
+    }
+}
---
+++ Algorithm.Sandbox/DynamicProgramming/Count/CountBinaryTree.cs
@@ -0,0 +1,48 @@
+﻿namespace Algorithm.Sandbox.DynamicProgramming.Count
+{
+    /// <summary>
+    /// Problem statement below
+    /// http://www.geeksforgeeks.org/find-all-possible-trees-with-given-inorder-traversal/
+    /// </summary>
+    public class CountBinaryTree
+    {
+        public static int Count(int n)
+        {
+            return Count(0, n);
+        }
+
+        /// <summary>
+        /// If we draw the BSTs we will see the count will be equal
+        /// to n'th Catalan number
+        /// And is same as the number of ways we can 
+        /// parenthesize an expression
+        /// </summary>
+        /// <param name="start"></param>
+        /// <param name="end"></param>
+        /// <returns></returns>
+        public static int Count(int start, int end)
+        {
+            if (start > end)
+            {
+                return 0;
+            }
+
+            //base case
+            if (start == end)
+            {
+                return 1;
+            }
+
+            var count = 0;
+
+            //break in to left & right
+            for (int i = start; i < end; i++)
+            {
+                count += Count(start, i)
+                         * Count(i + 1, end);
+            }
+
+            return count;
+        }
+    }
+}
