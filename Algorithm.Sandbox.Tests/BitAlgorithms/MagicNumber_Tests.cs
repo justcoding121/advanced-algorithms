a35de5a569be611d6334e24aceb86b609fb27bb8
justcoding121 <jehonathant@gmail.com>
2017-05-30 17:31:15 -0400

    add files

---
+++ Algorithm.Sandbox.Tests/BitAlgorithms/MagicNumber_Tests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm.Sandbox.Tests.BitAlgorithms
+{
+    [TestClass]
+    public class MagicNumber_Tests
+    {
+        [TestMethod]
+        public void SmokeTest()
+        {
+
+        }
+    }
+}
---
+++ Algorithm.Sandbox/BitAlgorithms/BaseConversion.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm.Sandbox.BitAlgorithms
+{
+    public class BaseConversion
+    {
+        public string Convert(string srcNumber, int srcBase,
+            int dstBase, char[] dstBaseChars)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
