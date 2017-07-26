43ec63ae1b74f57896cdc2567d97a5b5ee821ee7
justcoding121 <jehonathant@gmail.com>
2017-07-25 23:26:23 -0400

    optimal BST DP

--- Algorithm.Sandbox.Tests/DynamicProgramming/OptimalBST_Tests.cs
+++ Algorithm.Sandbox.Tests/DynamicProgramming/OptimalBST_Tests.cs
@@ -12,15 +12,26 @@ namespace Algorithm.Sandbox.Tests.DynamicProgramming
     /// Problem statement below
     /// http://www.geeksforgeeks.org/dynamic-programming-set-24-optimal-binary-search-tree/
     /// </summary>
+    [TestClass]
     public class OptimalBST_Tests
     {
-        //[TestMethod]
-        public void Smoke_Test()
+        [TestMethod]
+        public void OptimalBST_Smoke_Test()
         {
+            Assert.AreEqual(118, OptimalBST.FindOptimalCost(
+               new int[] { 10, 12 },
+               new int[] { 34, 50 }
+               ));
+
             Assert.AreEqual(142, OptimalBST.FindOptimalCost(
                 new int[] { 10, 12, 20 },
                 new int[] { 34, 8, 50 }
                 ));
+
+            Assert.AreEqual(415, OptimalBST.FindOptimalCost(
+              new int[] { 10, 12, 20, 12, 15, 18 },
+              new int[] { 34, 8, 50, 23, 50, 31 }
+              ));
         }
     }
 }
--- Algorithm.Sandbox/DynamicProgramming/OptimalBST.cs
+++ Algorithm.Sandbox/DynamicProgramming/OptimalBST.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,13 +8,46 @@ using System.Threading.Tasks;
 namespace Algorithm.Sandbox.DynamicProgramming
 {
     /// <summary>
-    /// Problem details below
+    /// Problem statement below
+    /// http://www.geeksforgeeks.org/dynamic-programming-set-24-optimal-binary-search-tree/
     /// </summary>
     public class OptimalBST
     {
         public static int FindOptimalCost(int[] keys, int[] freq)
         {
-            throw new NotImplementedException();
+            return FindOptimalCost(freq, 0, keys.Length - 1, 1, new Dictionary<string, int>());
+        }
+
+        public static int FindOptimalCost(int[] freq, 
+            int left, int right, int level, Dictionary<string, int> cache)
+        {
+            if (left > right)
+            {
+                return 0;
+            }
+
+            if (left == right)
+            {
+                return freq[left] * level;
+            }
+            var cacheKey = $"{left}-{right}-{level}";
+
+            if(cache.ContainsKey(cacheKey))
+            {
+                return cache[cacheKey];
+            }
+
+            var min = int.MaxValue;
+
+            for (int i = left; i <= right; i++)
+            {
+                min = Math.Min(min, FindOptimalCost(freq, 0, i - 1, level + 1, cache)
+                      + FindOptimalCost(freq, i + 1, right, level + 1, cache) + (freq[i] * level));
+            }
+
+            cache.Add(cacheKey, min);
+
+            return min;
         }
     }
 }
