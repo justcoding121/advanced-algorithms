0ae0f0329548764f1d0161fad785f998303b9b5c
justcoding121 <jehonathant@gmail.com>
2017-08-02 20:15:06 -0400

    add unchecked files

---
+++ Algorithm.Sandbox.Tests/BitAlgorithms/IntToBinary_Tests.cs
@@ -0,0 +1,21 @@
+﻿using Algorithm.Sandbox.BitAlgorithms;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm.Sandbox.Tests.BitAlgorithms
+{
+    [TestClass]
+    public class IntToBinary_Tests
+    {
+        [TestMethod]
+        public void IntToBinary_Smoke_Test()
+        {
+          Assert.AreEqual("00000001", IntToBinary.GetBinary(1));
+          Assert.AreEqual("11111111", IntToBinary.GetBinary(-1));
+        }
+    }
+}
---
+++ Algorithm.Sandbox/BitAlgorithms/IntToBinary.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm.Sandbox.BitAlgorithms
+{
+    public class IntToBinary
+    {
+        public static string GetBinary(int integer)
+        {
+            var stringBuilder = new StringBuilder();
+
+            for(int i = 7;i >=0; i--)
+            {
+                stringBuilder.Insert(0, (integer & 1) == 0 ? "0" : "1");
+                integer >>= integer;
+            }
+
+            return stringBuilder.ToString();
+        }
+    }
+}
