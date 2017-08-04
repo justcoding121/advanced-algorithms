efb654e5acf5ad122e8db3ea1f845b603045f04a
justcoding121 <jehonathant@gmail.com>
2017-08-03 22:07:05 -0400

    add more bit problems

---
+++ Algorithm.Sandbox/BitAlgorithms/FlipBitForLongest1Seq.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm.Sandbox.BitAlgorithms
+{
+    /// <summary>
+    /// You hvae an integer and you can flip exactly one bit from 0 to 1. Write code  
+    /// to find the length of longest sequence of 1s you can create
+    /// Example: 1775 (11011101111) => 8
+    /// </summary>
+    public class FlipBitForLongest1Seq
+    {
+    }
+}
---
+++ Algorithm.Sandbox/BitAlgorithms/NextSmallLargeNumber.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm.Sandbox.BitAlgorithms
+{
+    /// <summary>
+    /// Given a +ive integer, print the next smallest and the next largest number
+    /// that have the same number of 1 bits in their binary representation
+    /// </summary>
+    class NextSmallLargeNumber
+    {
+    }
+}
