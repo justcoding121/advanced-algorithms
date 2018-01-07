66474d67908be547361a58c328f7e4a0a520da4f
justcoding121 <jehonathant@gmail.com>
2018-01-06 21:31:19 -0500

    include selection option(r)  in permutation

--- Advanced.Algorithms/Combinatorics/Permutation.cs
+++ Advanced.Algorithms/Combinatorics/Permutation.cs
@@ -8,20 +8,20 @@ namespace Advanced.Algorithms.Combinatorics
 {
     public class Permutation
     {
-        public static List<List<T>> Find<T>(List<T> input, bool withRepetition = false)
+        public static List<List<T>> Find<T>(List<T> input, int r, bool withRepetition = false)
         {
             var result = new List<List<T>>();
 
-            Recurse(input, withRepetition, new List<T>(), new HashSet<int>(), result);
+            Recurse(input, r, withRepetition, new List<T>(), new HashSet<int>(), result);
 
             return result;
         }
 
-        private static void Recurse<T>(List<T> input, bool withRepetition,
+        private static void Recurse<T>(List<T> input, int r, bool withRepetition,
             List<T> prefix, HashSet<int> prefixIndices,
             List<List<T>> result)
         {
-            if (prefix.Count == input.Count)
+            if (prefix.Count == r)
             {
                 result.Add(new List<T>(prefix));
                 return;
@@ -37,53 +37,7 @@ namespace Advanced.Algorithms.Combinatorics
                 prefix.Add(input[j]);
                 prefixIndices.Add(j);
 
-                Recurse(input, withRepetition, prefix, prefixIndices, result);
-
-                prefix.RemoveAt(prefix.Count - 1);
-                prefixIndices.Remove(j);
-            }
-        }
-
-        public static List<List<T>> Find<T>(List<T> input, bool withRepetition,
-            bool withInversions) where T : IComparable
-        {
-            var result = new List<List<T>>();
-
-            Recurse(input, withRepetition, withInversions,
-                new List<T>(), new HashSet<int>(), result);
-
-            return result;
-        }
-
-        private static void Recurse<T>(List<T> input,
-            bool withRepetition, bool withInversions,
-            List<T> prefix, HashSet<int> prefixIndices,
-            List<List<T>> result) where T : IComparable
-        {
-            if (prefix.Count == input.Count
-                && (withInversions ||
-                (prefix.Count > 0 && prefix[0].CompareTo(prefix[prefix.Count - 1]) < 0)))
-            {
-                result.Add(new List<T>(prefix));
-                return;
-            }
-
-            if (prefix.Count == input.Count)
-            {
-                return;
-            }
-
-            for (int j = 0; j < input.Count; j++)
-            {
-                if (prefixIndices.Contains(j) && !withRepetition)
-                {
-                    continue;
-                }
-
-                prefix.Add(input[j]);
-                prefixIndices.Add(j);
-
-                Recurse(input, withRepetition, withInversions, prefix, prefixIndices, result);
+                Recurse(input, r, withRepetition, prefix, prefixIndices, result);
 
                 prefix.RemoveAt(prefix.Count - 1);
                 prefixIndices.Remove(j);
