ca47fd548b6271767d9a9510704d338830f35598
justcoding121 <jehonathant@gmail.com>
2018-04-26 10:18:49 -0400

    Update build credentials

--- .build/default.ps1
+++ .build/default.ps1
@@ -97,7 +97,7 @@ Task Document -depends Build {
 		git config --global credential.helper store
 		Add-Content "$HOME\.git-credentials" "https://$($env:github_access_token):x-oauth-basic@github.com`n"
 		git config --global user.email $env:github_email
-		git config --global user.name "justcoding121"
+		git config --global user.name "buildbot121"
 		git add . -A
 		git commit -m "Maintanance commit by build server"
 		git push origin master
