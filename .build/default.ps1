57d07f98c632846cc914745dcc6ed682b9169fa6
justcoding121 <jehonathant@gmail.com>
2018-04-25 20:54:40 -0400

    test build

--- .build/default.ps1
+++ .build/default.ps1
@@ -37,7 +37,6 @@ FormatTaskName (("-"*25) + "[{0}]" + ("-"*25))
 Task default -depends Clean, Build, Package, Document
 
 Task Document -depends Package{
-	
 	git config --global credential.helper store
 	Add-Content "$env:USERPROFILE\.git-credentials" "https://$($env:github_access_token):x-oauth-basic@github.com`n"
 	git config --global user.email %github_email%
