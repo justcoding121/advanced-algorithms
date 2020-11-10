268af5827043ed223a1af63f1b9c1c28e0cee62e
justcoding121 <jehonathant@gmail.com>
2020-11-09 18:09:28 -0700

    Update HopcroftKarp_Tests.cs

--- tests/Advanced.Algorithms.Tests/Graph/Matching/HopcroftKarp_Tests.cs
+++ tests/Advanced.Algorithms.Tests/Graph/Matching/HopcroftKarp_Tests.cs
@@ -46,7 +46,7 @@ namespace Advanced.Algorithms.Tests.Graph
         public void HopcroftKarp_AdjacencyMatrixGraph_Smoke_Test()
         {
 
-            var graph = new Advanced.Algorithms.DataStructures.Graph.AdjacencyMatrix.Graph<char>();
+            var graph = new Algorithms.DataStructures.Graph.AdjacencyMatrix.Graph<char>();
 
             graph.AddVertex('A');
             graph.AddVertex('B');
@@ -68,6 +68,37 @@ namespace Advanced.Algorithms.Tests.Graph
             graph.AddEdge('D', 'H');
             graph.AddEdge('E', 'F');
             graph.AddEdge('E', 'I');
+
+            var algorithm = new HopcroftKarpMatching<char>();
+
+            var result = algorithm.GetMaxBiPartiteMatching(graph);
+
+            Assert.AreEqual(result.Count, 4);
+
+        }
+
+        //[TestMethod]
+        public void HopcroftKarp_AdjacencyListGraph_Accurancy_Test_1()
+        {
+
+            var graph = new Advanced.Algorithms.DataStructures.Graph.AdjacencyList.Graph<char>();
+
+            graph.AddVertex('A');
+            graph.AddVertex('B');
+            graph.AddVertex('C');
+            graph.AddVertex('D');
+            graph.AddVertex('E');
+            graph.AddVertex('F');
+   
+            graph.AddEdge('A', 'D');
+            graph.AddEdge('A', 'E');
+            graph.AddEdge('A', 'F');
+            graph.AddEdge('B', 'D');
+            graph.AddEdge('B', 'E');
+            graph.AddEdge('B', 'F');
+            graph.AddEdge('C', 'D');
+            graph.AddEdge('C', 'E');
+            graph.AddEdge('C', 'F');
 
             var algorithm = new HopcroftKarpMatching<char>();
 
