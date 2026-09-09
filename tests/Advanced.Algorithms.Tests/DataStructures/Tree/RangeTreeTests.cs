66ab907d392543d69caab46cb8fbc4b9bde04a6e
Jehonathan Thomas <jehonathant@gmail.com>
2026-09-09 09:16:00 -0500

    Add RangeTree brute-force range-search oracle.

--- tests/Advanced.Algorithms.Tests/DataStructures/Tree/RangeTreeTests.cs
+++ tests/Advanced.Algorithms.Tests/DataStructures/Tree/RangeTreeTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using Advanced.Algorithms.DataStructures;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -111,6 +112,27 @@ namespace Advanced.Algorithms.Tests.DataStructures
             tree.Delete(new[] { 1 });
             Assert.AreEqual(0, tree.Count);
             Assert.AreEqual(0, tree.RangeSearch(new[] { 0 }, new[] { 10 }).Count);
+        }
+
+        [TestMethod]
+        public void RangeTree_Brute_Range_Oracle()
+        {
+            var tree = new RangeTree<int>(2);
+            var pts = new List<int[]>
+            {
+                new[] { 1, 2 }, new[] { 3, 4 }, new[] { 5, 1 }, new[] { 2, 8 }, new[] { 7, 7 }
+            };
+
+            foreach (var p in pts) tree.Insert(p);
+
+            var start = new[] { 0, 0 };
+            var end = new[] { 4, 5 };
+            var expected = pts.Count(p => p[0] >= start[0] && p[0] <= end[0] && p[1] >= start[1] && p[1] <= end[1]);
+            Assert.AreEqual(expected, tree.RangeSearch(start, end).Count);
+
+            tree.Delete(new[] { 3, 4 });
+            Assert.AreEqual(4, tree.Count);
+            Assert.AreEqual(1, tree.RangeSearch(start, end).Count);
         }
     }
 }
