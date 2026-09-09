cb4fc27447edf89cfdc913e80941ef1bb6f58b82
Jehonathan Thomas <jehonathant@gmail.com>
2026-09-09 08:50:48 -0500

    Add MinCut capacity cross-oracle against Edmonds-Karp max flow.

--- tests/Advanced.Algorithms.Tests/Graph/Cut/MinCut_Tests.cs
+++ tests/Advanced.Algorithms.Tests/Graph/Cut/MinCut_Tests.cs
@@ -173,6 +173,34 @@ namespace Advanced.Algorithms.Tests.Graph
             Assert.AreEqual(result.Count, 1);
         }
 
+        [TestMethod]
+        public void MinCut_Oracle_Capacity_Equals_MaxFlow()
+        {
+            var graph = new WeightedDiGraph<char, int>();
+            foreach (var v in "SABCDT") graph.AddVertex(v);
+            graph.AddEdge('S', 'A', 10);
+            graph.AddEdge('S', 'C', 10);
+            graph.AddEdge('A', 'B', 4);
+            graph.AddEdge('A', 'C', 2);
+            graph.AddEdge('A', 'D', 8);
+            graph.AddEdge('B', 'T', 10);
+            graph.AddEdge('C', 'D', 9);
+            graph.AddEdge('D', 'B', 6);
+            graph.AddEdge('D', 'T', 10);
+
+            var op = new EdmondKarpOperators();
+            var maxFlow = new EdmondKarpMaxFlow<char, int>(op).ComputeMaxFlow(graph, 'S', 'T');
+            var cut = new MinCut<char, int>(op).ComputeMinCut(graph, 'S', 'T');
+            var cutCap = 0;
+            foreach (var e in cut)
+            foreach (var oe in graph.GetVertex(e.Source).OutEdges)
+                if (oe.TargetVertexKey.Equals(e.Destination))
+                    cutCap += oe.Weight<int>();
+
+            Assert.AreEqual(maxFlow, cutCap);
+            Assert.AreEqual(19, cutCap);
+        }
+
         /// <summary>
         ///     operators for generics
         ///     implemented for int type for edge weights
