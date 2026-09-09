f57ae0457f81feb8567a8736ee596ff6a235dc5e
Jehonathan Thomas <jehonathant@gmail.com>
2026-09-09 09:15:55 -0500

    Add BST SortedSet insert/delete/enumerate oracle.
    Adversarial random ops cross-check membership, order, and BSTTester invariants.

--- tests/Advanced.Algorithms.Tests/DataStructures/Tree/BST_Tests.cs
+++ tests/Advanced.Algorithms.Tests/DataStructures/Tree/BST_Tests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using Advanced.Algorithms.DataStructures;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -308,6 +309,37 @@ namespace Advanced.Algorithms.Tests.DataStructures
             rightChild.Delete(2);
             Assert.IsFalse(rightChild.HasItem(2));
             Assert.IsTrue(rightChild.HasItem(3));
+        }
+
+        [TestMethod]
+        public void BST_SortedSet_Oracle()
+        {
+            var rnd = new Random(42);
+            var tree = new Bst<int>();
+            var set = new SortedSet<int>();
+
+            for (var t = 0; t < 500; t++)
+            {
+                var v = rnd.Next(0, 200);
+                if (set.Contains(v))
+                {
+                    tree.Delete(v);
+                    set.Remove(v);
+                }
+                else
+                {
+                    tree.Insert(v);
+                    set.Add(v);
+                }
+
+                Assert.AreEqual(set.Count, tree.Count);
+                CollectionAssert.AreEqual(set.ToList(), tree.ToList());
+                if (tree.Root != null)
+                {
+                    Assert.IsTrue(tree.Root.IsBinarySearchTree(int.MinValue, int.MaxValue));
+                    tree.Root.VerifyCount();
+                }
+            }
         }
     }
 }
