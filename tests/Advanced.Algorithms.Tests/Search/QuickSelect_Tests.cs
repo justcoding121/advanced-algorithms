902fece06dd630aef845f8b8a93790ae75012e77
Jehonathan Thomas <jehonathant@gmail.com>
2026-09-09 08:33:14 -0500

    Add QuickSelect oracle tests against sorted[k-1].

--- tests/Advanced.Algorithms.Tests/Search/QuickSelect_Tests.cs
+++ tests/Advanced.Algorithms.Tests/Search/QuickSelect_Tests.cs
@@ -49,5 +49,44 @@ namespace Advanced.Algorithms.Tests.Search
             Assert.AreEqual(1, QuickSelect<int>.FindSmallest(input, 1));
             Assert.AreEqual(3, QuickSelect<int>.FindSmallest(input, 5));
         }
+
+        [TestMethod]
+        public void QuickSelect_Oracle_Against_Sorted_K()
+        {
+            var rnd = new Random(7);
+            var fixtures = new[]
+            {
+                new[] { 9, 1, 5, 3, 7 },
+                new[] { 3, 1, 3, 2, 3 },
+                new[] { 4 },
+                new[] { 2, 2, 2, 2 },
+                new[] { 5, 4, 3, 2, 1, 0 },
+                new[] { -3, 10, -3, 0, 7, 10 }
+            };
+
+            foreach (var input in fixtures) AssertMatchesSortedK(input);
+
+            for (var t = 0; t < 40; t++)
+            {
+                var n = rnd.Next(1, 35);
+                var input = Enumerable.Range(0, n).Select(_ => rnd.Next(-50, 50)).ToArray();
+                AssertMatchesSortedK(input);
+            }
+        }
+
+        private static void AssertMatchesSortedK(int[] input)
+        {
+            var sorted = (int[])input.Clone();
+            Array.Sort(sorted);
+
+            for (var k = 1; k <= input.Length; k++)
+            {
+                var actual = QuickSelect<int>.FindSmallest((int[])input.Clone(), k);
+                Assert.AreEqual(sorted[k - 1], actual);
+            }
+
+            Assert.AreEqual(0, QuickSelect<int>.FindSmallest(input, 0));
+            Assert.AreEqual(0, QuickSelect<int>.FindSmallest(input, input.Length + 1));
+        }
     }
 }
